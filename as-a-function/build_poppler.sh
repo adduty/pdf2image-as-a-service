a0255a94ca0d829fece608e450f893e4e43276be
Andrew Duty <4403436-aduty@users.noreply.gitlab.com>
2021-05-26 14:28:27 -0600

    Working Dockerfile and poppler script for Alpine.

--- as-a-function/build_poppler.sh
+++ as-a-function/build_poppler.sh
@@ -3,10 +3,13 @@ mkdir -p poppler_binaries
 # Build the image
 docker build -t poppler-build .
 # Run the container
-docker run -d --name poppler-build-cont poppler-build sleep 20 
-#docker exec poppler-build-cont 
-sudo docker cp poppler-build-cont:/poppler_binaries .
+docker run -d --name poppler-build-cont poppler-build sleep 20
+#docker exec poppler-build-cont
+sudo docker cp poppler-build-cont:/pdf2image_layer .
 # Cleaning up
 docker kill poppler-build-cont
 docker rm poppler-build-cont
 docker image rm poppler-build
+
+# Bundle Lambda layer
+zip -r pdf2image_layer.zip pdf2image_layer
